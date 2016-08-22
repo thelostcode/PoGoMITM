51597faa6e95e62dd523b13b041063ba5b56dec8
Taras Bulba <tbulba.db@gmail.com>
2016-08-22 19:19:53 +0300

    Some changes

---
+++ PoGo Proxy/PoGoWebSession.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Titanium.Web.Proxy.Http;
+
+namespace PoGo_Proxy
+{
+    public class PoGoWebSession
+    {
+        public string Uri { get; set; }
+        public Guid Guid { get; set; }
+        public Request RawRequest { get; set; }
+        public byte[] RequestBody { get; set; }
+        public Response RawResponse { get; set; }
+        public byte[] ResponseBody { get; set; }
+        public ulong RequestId { get; set; }
+        public MessageBlock RequestBlock { get; set; }
+        public MessageBlock ResponseBlock { get; set; }
+    }
+}
