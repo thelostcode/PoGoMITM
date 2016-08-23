9ded32759252d06100d0e29692a3ad02eb4e722f
Taras Bulba <tbulba.db@gmail.com>
2016-08-23 10:14:01 +0300

    Cleanup

--- PoGo Proxy.Sample/Program.cs
+++ PoGo Proxy.Sample/Program.cs
@@ -1,33 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Google.Protobuf;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
 using PoGo_Proxy.Logging;
 using PoGo_Proxy.Models;
-using POGOProtos.Map;
-using POGOProtos.Map.Pokemon;
-using POGOProtos.Networking.Requests;
-using POGOProtos.Networking.Responses;
 
 namespace PoGo_Proxy.Sample
 {
     internal class Program
     {
         private static readonly HashSet<string> HostsToLog = new HashSet<string> { "sso.pokemon.com", "pgorelease.nianticlabs.com" };
-        private static List<ILogger> _loggers = new List<ILogger>();
+        private static readonly List<ILogger> Loggers = new List<ILogger>();
 
         private static void Main()
         {
             Console.WriteLine("Hit any key to stop the proxy and exit..");
             Console.WriteLine();
 
-            _loggers.Add(new FileLogger());
-            _loggers.Add(new MongoLogger("mongodb://127.0.0.1/PoGoMITM?maxPoolSize=1000&minPoolSize=100"));
+            Loggers.Add(new FileLogger());
+            Loggers.Add(new MongoLogger("mongodb://127.0.0.1/PoGoMITM?maxPoolSize=1000&minPoolSize=100"));
 
             var controller = new ProxyController("0.0.0.0", 8080) { Out = Console.Out };
 
@@ -53,7 +42,7 @@ namespace PoGo_Proxy.Sample
             if (!HostsToLog.Contains(webRequest.Uri.Host)) return;
             Console.WriteLine(webRequest.Uri.AbsoluteUri + " Request Completed.");
 
-            foreach (var logger in _loggers)
+            foreach (var logger in Loggers)
             {
                 await logger.Log(webRequest);
             }
--- PoGo Proxy/Logging/ILogger.cs
+++ PoGo Proxy/Logging/ILogger.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using PoGo_Proxy.Models;
 
 namespace PoGo_Proxy.Logging
